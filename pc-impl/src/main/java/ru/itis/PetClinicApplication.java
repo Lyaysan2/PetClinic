2eda3272a92584f1d7e6d9dc8742b1653991c777
Lyaysan2 <70819092+Lyaysan2@users.noreply.github.com>
2023-03-13 17:12:00 +0300

    test for heroku

--- pc-impl/src/main/java/ru/itis/PetClinicApplication.java
+++ pc-impl/src/main/java/ru/itis/PetClinicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication()
 public class PetClinicApplication {
 
     public static void main(String[] args) {
